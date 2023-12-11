ad82d20dfb1f0b605c3287f152c7fbc671e6e802
Liiked <linyuanqianpp@126.com>
2023-12-12 03:22:23 +0800

    feat: 第二部分80%

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import "./App.css";
 import styles from "./App.module.scss";
 import Back1 from "./assets/imgs/header-background_1.png";
 import Logo1 from "./assets/imgs/header__logo_1.png";
 import Logo2 from "./assets/imgs/header__logo_2.png";
+import ActivityBack from "./assets/imgs/activity_header.png";
+import Activity1 from "./assets/imgs/activity_1.png";
+import Activity2 from "./assets/imgs/activity_2.png";
+import cls from "classnames";
 
 function App() {
   return (
@@ -35,7 +40,56 @@ function App() {
             </div>
           </div>
         </section>
-        <section className="activity"></section>
+        <section className={styles["activity"]}>
+          <div className={styles["head"]}>
+            <img src={ActivityBack} alt="" />
+            <div className={styles["description"]}>
+              凡在12月15日至28日期间 <br /> 凭此活动页面购买指定产品的顾客
+              <br />
+              即可
+              <span className={styles["red-text"]}>获赠“指定版活动产品"</span>。
+            </div>
+          </div>
+          <div className={styles["body"]}>
+            <div className={styles["product-card"]}>
+              <div className={styles["title"]}>活动1</div>
+              <div className={styles["pic"]}>
+                <img src={Activity1} alt="" />
+              </div>
+              <div className={styles["content"]}>
+                <ol>
+                  <li>
+                    购买：焕白亮肤肌底精华 (啫喱) 60g+替换套装，价格25,400日元
+                    (含税27,940日元) 的顾客
+                  </li>
+                  <li>赠送：焕白亮肤肌底精华 (啫喱) 7g (迷你) x 2 瓶</li>
+                </ol>
+              </div>
+            </div>
+            <div className={styles["product-card"]}>
+              <div className={styles["title"]}>活动2</div>
+              <div className={styles["pic"]}>
+                <img src={Activity2} alt="" />
+              </div>
+              <div className={styles["content"]}>
+                <ol>
+                  <li>
+                    购买：焕白亮肤肌底精华 (啫喱) 60g+替换套装，价格25,400日元
+                    (含税27,940日元) 的顾客
+                  </li>
+                  <li>赠送：焕白亮肤肌底精华 (啫喱) 7g (迷你) x 2 瓶</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className={styles["foot"]}>
+            <h2 className={styles["activity-shop-name"]}>活动店铺</h2>
+            <ol>
+              <li>ASTALIFT直营店：银座店、六本木店</li>
+              <li>杉木药店：浅草店、心斋桥店、新宿三丁目店</li>
+            </ol>
+          </div>
+        </section>
         <section className="description-film"></section>
         <section className="description-human"></section>
         <section className="description-shrimp"></section>

b7a1821f693048618de02dda007cf32fdadca52c
liiked <linyuanqianpp@126.com>
2025-09-14 15:26:37 +0800

    feat: 页面4

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,6 +4,7 @@ import styles from "./Main.module.scss";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
+import Section4 from "./Section4";
 
 export default function Main() {
   useEffect(() => {
@@ -14,6 +15,7 @@ export default function Main() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
